8cfc85069e7eed5a7fa09aad1830dba85389b52f
robinson29041986 <robinson.rodriguez38@misena.edu.co>
2022-10-04 20:09:37 -0500

    New Added Imports

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import express from 'express';
-import ProductsRoutes from './routes/ProductsRoutes.js';
-import CategoriesRoutes from './routes/CategoriesRoutes.js';
+import express from "express";
+import ProductsRoutes from "./routes/ProductsRoutes.js";
+import CategoriesRoutes from "./routes/CategoriesRoutes.js";
+import CustomersRoutes from "./routes/CustomersRoutes.js";
 
 const App = express();
 
@@ -9,5 +10,6 @@ App.use(express.json());
 
 App.use(ProductsRoutes);
 App.use(CategoriesRoutes);
+App.use(CustomersRoutes);
 
 export default App;
