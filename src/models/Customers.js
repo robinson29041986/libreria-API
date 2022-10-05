72678358672ec762afc4bb3a05a725e11b48e119
robinson29041986 <robinson.rodriguez38@misena.edu.co>
2022-10-04 21:21:12 -0500

    Update Model

--- src/models/Customers.js
+++ src/models/Customers.js
@@ -1,57 +1,61 @@
-import { sequelize } from '../database/Database.js';
-import { DataTypes } from 'sequelize';
-import { Orders } from './Orders.js';
+import { sequelize } from "../database/Database.js";
+import { DataTypes } from "sequelize";
+import { Orders } from "./Orders.js";
 
-export const Customers = sequelize.define('customers', {
+export const Customers = sequelize.define(
+  "customers",
+  {
     id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true
+      type: DataTypes.INTEGER,
+      primaryKey: true,
     },
     name: {
-        type: DataTypes.STRING(50),
-        allowNull: false
+      type: DataTypes.STRING(50),
+      allowNull: false,
     },
     last_name: {
-        type: DataTypes.STRING(50),
-        allowNull: false
+      type: DataTypes.STRING(50),
+      allowNull: false,
     },
     birthday: {
-        type: DataTypes.DATEONLY,
-        allowNull: false
+      type: DataTypes.DATEONLY,
+      allowNull: false,
     },
     cellphone: {
-        type: DataTypes.STRING,
-        allowNull: false
+      type: DataTypes.STRING,
+      allowNull: false,
     },
     email: {
-        type: DataTypes.TEXT,
-        allowNull: false
+      type: DataTypes.TEXT,
+      allowNull: false,
     },
     password: {
-        type: DataTypes.TEXT,
-        allowNull: false
-    }
-}, {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+  },
+  {
     /* Personalización del timestamps */
     timestamps: true,
-    createdAt: 'created_at',
-    updatedAt: 'updated_at'
-});
+    createdAt: "created_at",
+    updatedAt: "updated_at",
+  }
+);
 
 Customers.hasMany(Orders, {
-    /* Declaración de la asociación */
-    foreignKey: {
-        name: 'customers_dni',
-        allowNull: false,
-    },
-    sourceKey: 'dni',
-    /* Acciones de actualización y borrado */
-    onDelete: 'SET NULL',
-    onUpdate: 'CASCADE'
+  /* Declaración de la asociación */
+  foreignKey: {
+    name: "customers_id",
+    allowNull: false,
+  },
+  sourceKey: "id",
+  /* Acciones de actualización y borrado */
+  onDelete: "SET NULL",
+  onUpdate: "CASCADE",
 });
 
 Orders.belongsTo(Customers, {
-    /* Declaración de la asociación */
-    foreignKey: 'customers_dni',
-    targetId: 'dni'
+  /* Declaración de la asociación */
+  foreignKey: "customers_id",
+  targetId: "id",
 });
