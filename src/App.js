1b0ffb4b59a67c2e68c8006e7858ea0e3e26244f
robinson29041986 <robinson.rodriguez38@misena.edu.co>
2022-10-28 19:17:07 -0500

    Updated Files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
+
+/* Importacion de Rutas */
+
 import ProductsRoutes from "./routes/ProductsRoutes.js";
 import CategoriesRoutes from "./routes/CategoriesRoutes.js";
 import UsersRoutes from "./routes/UsersRoutes.js";
 import SalesOrderRoutes from "./routes/SalesOrderRoutes.js";
-import CartRoutes from "./routes/CartRoutes.js"
-import PaymentRoutes from "./routes/PaymentRoutes.js"
+import CartRoutes from "./routes/CartRoutes.js";
+import PaymentRoutes from "./routes/PaymentRoutes.js";
+import Auths from "./routes/AuthRoutes.js";
 
 dotenv.config();
 const App = express();
@@ -13,12 +17,14 @@ const App = express();
 // Middlewares
 App.use(express.json());
 
+App.use(Auths);
 App.use(ProductsRoutes);
 App.use(CategoriesRoutes);
 App.use(UsersRoutes);
 App.use(SalesOrderRoutes);
 App.use(CartRoutes);
 App.use(PaymentRoutes);
+
 
 /* Error 404 endpoint */
 
