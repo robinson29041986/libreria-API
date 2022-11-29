51b092c208c32376b80b494cfa033978d5bd6aaf
robinson29041986 <robinson.rodriguez38@misena.edu.co>
2022-11-28 20:59:56 -0500

    Added Multer Middlewares

---
+++ src/middlewares/ImageUploader.js
@@ -0,0 +1,26 @@
+import multer from 'multer';
+import path from 'path';
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, './public/uploads/')
+    },
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + path.extname(file.originalname))
+    }
+});
+
+export const upload = multer({
+    storage: storage,
+    limits: { fileSize: '1000000' },
+    fileFilter: (req, file, cb) => {
+        const fileTypes = /jpeg|jpg|png/
+        const mimeType = fileTypes.test(file.mimetype)
+        const extname = fileTypes.test(path.extname(file.originalname))
+
+        if (mimeType && extname) {
+            return cb(null, true)
+        }
+        cb('Eliga un formato compatible de imagen para cargar.')
+    }
+}).single('image')
