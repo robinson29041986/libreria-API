e015b32fe0a4ff0c57064d09265a5623ea35a9db
robinson29041986 <robinson.rodriguez38@misena.edu.co>
2022-11-28 20:59:27 -0500

    Added Cors Middlewares

---
+++ src/middlewares/CorsOptions.js
@@ -0,0 +1,18 @@
+const whitelist = ['http://localhost:3000', 'http://localhost:5000'];
+
+const corsOptions = {
+  allowedHeards: [
+    'Origin',
+    'X-Requested-With',
+    'Context-Type',
+    'Accept',
+    'X-Access-Token',
+  ],
+  credentials: true,
+  methods: 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
+  origin: whitelist,
+  preflightContinue: false,
+  optionsSuccessStatus: 200
+};
+
+export default corsOptions;
